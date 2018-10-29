0d0a1f4fac10129ef27565e1e80c2f3c5385b26a
Daniel Bergstein <db102010@aol.com>
2018-10-29 14:41:39 -0400

    Api request searches for elon musk tweets and filters out retweets/replies

--- twitter_api_request.py
+++ twitter_api_request.py
@@ -1,14 +1,29 @@
 from searchtweets import ResultStream, gen_rule_payload, load_credentials, collect_results
-# premium_search_args = load_credentials("~/.twitter_keys.yaml",
-#                                        yaml_key="search_tweets_premium",
-#                                        env_overwrite=False)
+
+
+def removeRetweetsReplies(tweets):
+    filteredTweets = []
+    for tweet in tweets:
+        if tweet.in_reply_to_user_id == None:
+            filteredTweets.append(tweet)
+    return filteredTweets
 
 premium_search_args = load_credentials("/Users/Daniel/OneDrive/Columbia/3rd Semester/Other/twitterStocks/Twitter_Stocks/twitter_keys.yaml",
                                        yaml_key="twitter_keys",
                                        env_overwrite=False)
-rule = gen_rule_payload("beyonce", results_per_call=100) # testing with a sandbox account
-tweets = collect_results(rule,
-                         max_results=100,
-                         result_stream_args=premium_search_args)
-print(rule)
-[print(tweet.all_text) for tweet in tweets[0:10]];
+rule = gen_rule_payload("from:elonmusk", results_per_call=100) # testing with a sandbox account
+
+
+rs = ResultStream(rule_payload=rule,
+                  max_results=500,
+                  max_pages=1,
+                  **premium_search_args)
+tweets = list(rs.stream())
+
+
+filteredTweets = removeRetweetsReplies(tweets)
+
+[print(tweet.created_at_datetime) for tweet in filteredTweets];
+[print(tweet.all_text, end='\n\n') for tweet in filteredTweets];
+print(len(filteredTweets))
+
