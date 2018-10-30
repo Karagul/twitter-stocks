fbb04db48794d486f9fbf406939c141e1b056c91
Daniel Bergstein <db102010@aol.com>
2018-10-30 01:52:08 -0400

    Easier to read. Added data file.

--- twitter_api_request.py
+++ twitter_api_request.py
@@ -1,29 +1,30 @@
-from searchtweets import ResultStream, gen_rule_payload, load_credentials, collect_results
+from searchtweets import ResultStream, gen_rule_payload, load_credentials
 
 
 def removeRetweetsReplies(tweets):
     filteredTweets = []
     for tweet in tweets:
-        if tweet.in_reply_to_user_id == None:
+        if tweet.in_reply_to_user_id is None:
             filteredTweets.append(tweet)
     return filteredTweets
 
 premium_search_args = load_credentials("/Users/Daniel/OneDrive/Columbia/3rd Semester/Other/twitterStocks/Twitter_Stocks/twitter_keys.yaml",
                                        yaml_key="twitter_keys",
                                        env_overwrite=False)
-rule = gen_rule_payload("from:elonmusk", results_per_call=100) # testing with a sandbox account
 
-
-rs = ResultStream(rule_payload=rule,
-                  max_results=500,
-                  max_pages=1,
-                  **premium_search_args)
+rule = gen_rule_payload("from:elonmusk", results_per_call=100)
+rs = ResultStream(rule_payload=rule, max_results=100, max_pages=1, **premium_search_args)
 tweets = list(rs.stream())
-
-
 filteredTweets = removeRetweetsReplies(tweets)
 
-[print(tweet.created_at_datetime) for tweet in filteredTweets];
+
+with open("data.csv", "w") as file:
+    for time in [tweet.created_at_datetime for tweet in filteredTweets]:
+        file.write(str(time))
+        file.write("\n")
+
+
 [print(tweet.all_text, end='\n\n') for tweet in filteredTweets];
-print(len(filteredTweets))
+
+
 
